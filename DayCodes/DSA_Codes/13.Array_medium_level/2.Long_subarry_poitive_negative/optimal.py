3a305c34065a39ebecbd0c94abab620552dc7885
Adarshkhotak <167761889+Adarshkhotak@users.noreply.github.com>
2026-08-19 23:47:39 +0530

    Update optimal.py

--- DayCodes/DSA_Codes/13.Array_medium_level/2.Long_subarry_poitive_negative/optimal.py
+++ DayCodes/DSA_Codes/13.Array_medium_level/2.Long_subarry_poitive_negative/optimal.py
@@ -13,16 +13,24 @@ def getLongestSubarray(nums: List[int], k: int) -> int:
     sum_dict=dict()
     max_len=0
     sum=0
+    start, end= -1,-1
     for i in range(0,n):
         sum=sum+nums[i]
         if sum==k:
             max_len=i+1
+            start=0
+            end=i
         
         rem=sum-k
         if rem in sum_dict:
             ln=i-sum_dict[rem]
-            max_len=max(max_len,ln)
+            if ln > max_len:
+                max_len=ln
+                strat= sum_dict[rem]+1
+                end=i
         
         if sum not in sum_dict:
             sum_dict[sum]=i
-    return max_len
+    if start==-1 and end ==-1:
+        return []
+    return max_len , nums[start : end+=1]
